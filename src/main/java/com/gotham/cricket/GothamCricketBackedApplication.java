487da0e6132f112bb729473ccc5aac484d212b9a
shiva prasad <shivaprasadnyc@gmail.com>
2026-04-26 17:34:44 -0400

    new database auto create admin shiva

--- src/main/java/com/gotham/cricket/GothamCricketBackedApplication.java
+++ src/main/java/com/gotham/cricket/GothamCricketBackedApplication.java
@@ -1,7 +1,14 @@
 package com.gotham.cricket;
 
+import com.gotham.cricket.entity.User;
+import com.gotham.cricket.enums.Role;
+import com.gotham.cricket.enums.UserStatus;
+import com.gotham.cricket.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class GothamCricketBackedApplication {
@@ -9,5 +16,33 @@ public class GothamCricketBackedApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GothamCricketBackedApplication.class, args);
 	}
+
+
+
+
+
+    @Bean
+    public CommandLineRunner createAdmin(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+
+            if (userRepository.findByEmail("shiva_prem14@hotmail.com").isEmpty()) {
+
+                User admin = new User();
+                admin.setFirstName("shiva");
+                admin.setLastName("prasad");
+                admin.setFullName("shiva prasad");
+                admin.setEmail("shiva_prem14@hotmail.com");
+                admin.setPassword(passwordEncoder.encode("admin123"));
+                admin.setRole(Role.ADMIN);
+                admin.setStatus(UserStatus.APPROVED);
+
+                userRepository.save(admin);
+
+                System.out.println("✅ Admin user created successfully");
+            } else {
+                System.out.println("ℹ️ Admin already exists");
+            }
+        };
+    }
 
 }
